197593c7463b9ebb33f77e21d42249c557dc5900
akalisz <kaliszadam99@gmail.com>
2020-11-09 18:41:21 +0100

    Simulation done

--- wolf_and_sheep.py
+++ wolf_and_sheep.py
@@ -3,7 +3,7 @@ from sheep import Sheep
 
 # params init
 init_pos_limit = 10.0
-epochs = 70
+epochs = 50
 sheep_count = 15
 sheep_move_dist = 0.5
 wolf_move_dist = 1.0
@@ -15,24 +15,39 @@ flock = []
 for i in range(sheep_count):
     flock.append(Sheep(sheep_move_dist, i, init_pos_limit))
 
-log = '[{}] WOLF: {:.3f}, {:.3f}, FLOCK: {}'
+# log formatter
+epoch_log = '[{:2}]\twolf: ({: >7.3f}, {: >7.3f})\tno sheep: {:2}'
+eaten_log = '  •-——— sheep [{:2}] is dead'
+print(epoch_log.format(0, wolf.get_pos()[0], wolf.get_pos()[1], len(flock)))
 
-while len(flock) > 0 and epochs > 0:
-    epochs -= 1
+# main loop
+for i in range(1, epochs+1):
 
+    # move and update sheep
     for sheep in flock:
         sheep.move()
         sheep.update(wolf.get_pos())
 
+    # find closest sheep to wolf
     closest_sheep = flock[0]
     for sheep in flock:
         if sheep.wolf_dist < closest_sheep.wolf_dist:
             closest_sheep = sheep
 
-    print(log.format(epochs, wolf.get_pos()[0], wolf.get_pos()[1], len(flock)))
+    # eat or chase sheep
     if closest_sheep.wolf_dist < wolf.move_dist:
+        print(eaten_log.format(closest_sheep.id))
         flock.remove(closest_sheep)
-
     else:
         wolf.move(closest_sheep.get_pos())
 
+    # print epoch summary
+    print(epoch_log.format(i,
+                           wolf.get_pos()[0],
+                           wolf.get_pos()[1],
+                           len(flock))
+          )
+
+    # end loop if no sheep in flock
+    if len(flock) < 1:
+        break

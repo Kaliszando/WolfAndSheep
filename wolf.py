cc2034e446f780c766f9aa0ad325c97f87f5bf86
akalisz <kaliszadam99@gmail.com>
2020-11-22 19:00:41 +0100

    added logging

--- wolf.py
+++ wolf.py
@@ -1,4 +1,5 @@
 import functions
+import logging
 
 
 class Wolf:
@@ -6,6 +7,8 @@ class Wolf:
         self._x = init_pos_x
         self._y = init_pos_y
         self.move_dist = wolf_move_dist
+        logging.debug('Wolf.__init__({}, {}, {}) returns: {}'.format(wolf_move_dist, init_pos_x, init_pos_x, None))
+        logging.info('object of Wolf created on position {}'.format((self._x, self._y)))
 
     def move(self, sheep_pos):
         x = functions.euclidean_dist((self._x, self._y), sheep_pos)
@@ -13,8 +16,12 @@ class Wolf:
         norm_vec = functions.vec_norm(vec, x)
         new_x = self._x + norm_vec[0] * self.move_dist
         new_y = self._y + norm_vec[1] * self.move_dist
+        logging.info('Wolf.move() Wolf moved from {} to {}'.format((self._x, self._y), (new_x, new_y)))
         self._x = new_x
         self._y = new_y
+        logging.debug('Wolf.move({}) returns: {}'. format(sheep_pos, None))
 
     def get_pos(self):
+        logging.debug('Wolf.get_pos() returns: {}'.format((self._x, self._y)))
+        logging.info('Wolf.get_pos() pos:{}'.format((self._x, self._y)))
         return self._x, self._y
--- wolf_and_sheep.py
+++ wolf_and_sheep.py
@@ -1,8 +1,10 @@
 import csv
+import datetime
 import json
 import argparse
 import os
 import configparser
+import logging
 
 from wolf import Wolf
 from sheep import Sheep
@@ -34,7 +36,10 @@ sheep_count = 15
 sheep_move_dist = 0.5
 wolf_move_dist = 1.0
 
-log_dir = os.getcwd()
+log_lvl = None
+
+# path to pos.json and alive.csv files
+data_dir = os.getcwd()
 
 # parse arguments
 parser = argparse.ArgumentParser()
@@ -48,12 +53,13 @@ parser.add_argument('-d', '--dir',
                     type=dir_type,
                     metavar='DIR',
                     help='specify path to catalog that holds generated files',
-                    dest='log_dir'
+                    dest='data_dir'
                     )
 parser.add_argument('-l', '--log',
                     type=int,
                     metavar='LEVEL',
-                    help='choose level of logs saved in chase.log file',
+                    help='choose level of logs saved in chase.log file: 10: DEBUG, 20: INFO, 30: WARNING, 40: ERROR, '
+                         '50: CRITICAL',
                     dest='log_lvl'
                     )
 parser.add_argument('-r', '--rounds',
@@ -66,8 +72,7 @@ parser.add_argument('-s', '--sheep',
                     type=int,
                     metavar='NUM',
                     dest='sheep_no',
-                    help='specify number of sheep in flock: 10: DEBUG, 20: INFO, '
-                         '30: WARNING, 40: ERROR, 50: CRITICAL'
+                    help='specify number of sheep in flock'
                     )
 parser.add_argument('-w', '--wait',
                     action='store_true',
@@ -85,6 +90,24 @@ args = parser.parse_args()
 wait_flag = args.wait_flag
 print_flag = args.quiet_flag
 
+if args.data_dir:
+    data_dir = os.path.join(data_dir, args.data_dir)
+
+if args.log_lvl:
+    for lvl in [10, 20, 30, 40, 50]:
+        if args.log_lvl == lvl:
+            log_lvl = lvl
+
+    if log_lvl is None:
+        raise ValueError('no such level: ' + str(args.log_lvl))
+
+    print(os.path.join(data_dir, 'chase.log'))
+
+    logging.basicConfig(filename=os.path.join(data_dir, 'chase.log'),
+                        filemode='w',
+                        level=log_lvl
+                        )
+
 if args.conf_file:
     config = configparser.ConfigParser()
     config.read(args.conf_file)
@@ -93,28 +116,33 @@ if args.conf_file:
     movement = config['Movement']
 
     if float(terrain['InitPosLimit']) < 0:
+        logging.critical('ValueError raised: incorrect value in InitPosLimit config file')
         raise ValueError('negative value in config file: InitPosLimit')
+
     if float(movement['SheepMoveDist']) < 0:
+        logging.critical('ValueError raised: incorrect value in SheepMoveDist config file')
         raise ValueError('negative value in config file: SheepMoveDist')
+
     if float(movement['WolfMoveDist']) < 0:
+        logging.critical('ValueError raised: incorrect value in WolfMoveDist config file')
         raise ValueError('negative value in config file: WolfMoveDist')
 
     init_pos_limit = float(terrain['InitPosLimit'])
     sheep_move_dist = float(movement['SheepMoveDist'])
     wolf_move_dist = float(movement['WolfMoveDist'])
 
-if args.log_dir:
-    log_dir = os.path.join(log_dir, args.log_dir)
 
-if args.log_lvl:
-    # TODO
-    pass
+if args.rounds_no:
+    if args.rounds_no > 0:
+        epochs = args.rounds_no
+    elif log_lvl is not None:
+        logging.warning('value passed by -r/--rounds is incorrect, continuing with default values')
 
-if args.rounds_no and args.rounds_no >= 0:
-    epochs = args.rounds_no
-
-if args.sheep_no and args.sheep_no > 0:
-    sheep_count = args.sheep_no
+if args.sheep_no:
+    if args.sheep_no > 0:
+        sheep_count = args.sheep_no
+    elif log_lvl is not None:
+        logging.warning('value passed by -s/--sheep is incorrect, continuing with default values')
 
 # sheep list
 flock = []
@@ -131,18 +159,21 @@ for i in range(sheep_count):
 # log formatter
 epoch_log = '[{:2}]\twolf pos.: ({: >7.3f}, {: >7.3f})\tnumber of sheep: {:2}'
 eaten_log = '  •———— sheep [{:2}] is dead'
+tmp_str = epoch_log.format(0, wolf.get_pos()[0], wolf.get_pos()[1], len(flock))
 if print_flag:
-    print(epoch_log.format(0, wolf.get_pos()[0], wolf.get_pos()[1], len(flock)))
+    print(tmp_str)
+logging.info(tmp_str)
 
 # init alive.csv file
-with open(os.path.join(log_dir, 'alive.csv'), mode='w', newline="") as csv_file:
+with open(os.path.join(data_dir, 'alive.csv'), mode='w', newline="") as csv_file:
     fieldnames = ['epoch_no', 'living_sheep']
     writer = csv.writer(csv_file)
     writer.writerow(fieldnames)
+    logging.info('created alive.csv')
 
 # init pos.json file
-with open(os.path.join(log_dir, 'pos.json'), mode='w', newline='') as json_file:
-    pass
+with open(os.path.join(data_dir, 'pos.json'), mode='w', newline='') as json_file:
+    logging.info('created empty pos.json')
 
 # wait for user input
 if wait_flag:
@@ -162,42 +193,52 @@ for i in range(1, epochs + 1):
     for sheep in flock:
         if sheep.wolf_dist < closest_sheep.wolf_dist:
             closest_sheep = sheep
+    logging.info('sheep closest to wolf found ({})'.format(closest_sheep.get_pos()))
 
     # eat or chase sheep
     if closest_sheep.wolf_dist < wolf.move_dist:
+        tmp_str = eaten_log.format(closest_sheep.id)
         if print_flag:
-            print(eaten_log.format(closest_sheep.id))
+            print(tmp_str)
+        logging.info(tmp_str)
+
         flock.remove(closest_sheep)
         sheep_pos[closest_sheep.id] = None
     else:
         wolf.move(closest_sheep.get_pos())
+        logging.info('Wolf is chasing closest sheep')
 
     # print epoch summary
-    if print_flag:
-        print(epoch_log.format(i,
+    tmp_str = epoch_log.format(i,
                                wolf.get_pos()[0],
                                wolf.get_pos()[1],
                                len(flock))
-              )
+    if print_flag:
+        print(tmp_str)
+    logging.info(tmp_str)
 
     # append data to csv file
-    with open(os.path.join(log_dir, 'alive.csv'), mode='a', newline="") as csv_file:
+    with open(os.path.join(data_dir, 'alive.csv'), mode='a', newline="") as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow([i, len(flock)])
+        logging.info('added row to alive.csv: ({}, {})'.format(i, len(flock)))
 
     # append data to json file
-    with open(os.path.join(log_dir, 'pos.json'), mode='a', newline='') as json_file:
+    with open(os.path.join(data_dir, 'pos.json'), mode='a', newline='') as json_file:
         json_row = {
             'round_no': i,
             'wolf_pos': wolf.get_pos(),
             'sheep_pos': sheep_pos
         }
         json_file.write(json.dumps(json_row, indent=4))
+        logging.info('added row to pos.json')
 
     # end loop if no sheep in flock
     if len(flock) < 1:
+        logging.info('simulation ended: all sheep were eaten')
         break
 
     # wait for user input
     if wait_flag and input('Press enter to continue: ') == 'exit':
+        logging.info('simulation ended: user stopped the simulation')
         break
